9857feabf3ea024a1c61ea1f5cda6bca2921644b
Matthew Davila <matthew.davila@att.net>
2019-03-20 23:59:23 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,12 +1,20 @@
-function basicTeenager(age) {
+function basicTeenager(age) {if (age>=13 && age<=19)
+    return "You are a teenager!"
 
 }
 
-function teenager(age) {
+function teenager(age) {if (age>=13 && age<=19)
+    {return "You are a teenager!"}
+    return "You are not a teenager"
 
 }
 
-function ageChecker(age) {
+function ageChecker(age) {if (age>=13 && age<=19)
+  {return "You are a teenager!"}
+  else if (age<13)
+  {return "You are a kid"}
+  else
+  {return "You are a grownup"}
 
 }
 
