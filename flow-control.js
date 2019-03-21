32664b926073b3eff4394c2f6b089740c435b9e2
Matthew Davila <matthew.davila@att.net>
2019-03-21 00:39:42 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -32,3 +32,4 @@ function switchAge(age) {switch (age){
     case 17: return phrase break;
     case 18: return phrase break;
     case 19: return phrase break;
+    default: return "You have an age"
