35099da5b716e014e3b702bad2495a1283b31e91
Matthew Davila <matthew.davila@att.net>
2019-03-21 01:23:42 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,5 +1,5 @@
 function basicTeenager(age) {if (age>=13 && age<=19)
-    return ("You are a teenager!")
+    return "You are a teenager!"
 
 }
 
@@ -22,14 +22,13 @@ function ternaryTeenager(age) {return (age>=13 && age<=19) ?
     "You are a teenager":"You are not a teenager"
 
 }
-var phrase;
-phrase = "You are a teenager"
-function switchAge(age) {switch (age){
-    case 13: return phrase break;
-    case 14: return phrase break;
-    case 15: return phrase break;
-    case 16: return phrase break;
-    case 17: return phrase break;
-    case 18: return phrase break;
-    case 19: return phrase break;
-    default: return "You have an age"}}
+var phrase = "You are a teenager"
+function switchAge(age) {switch (age) {
+    case 13: return phrase; break;
+    case 14: return phrase; break;
+    case 15: return phrase; break;
+    case 16: return phrase; break;
+    case 17: return phrase; break;
+    case 18: return phrase; break;
+    case 19: return phrase; break;
+    default: return "You have an age"}
